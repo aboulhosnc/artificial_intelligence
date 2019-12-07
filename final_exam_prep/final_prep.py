fdd4265fb4f69d9bd1aa8c0dce1dfe5624a9770c
aboulhosnc <aboulhosnc@gmail.com>
2019-12-07 17:49:17 -0500

    started at school with weights

--- final_exam_prep/final_prep.py
+++ final_exam_prep/final_prep.py
@@ -1,6 +1,12 @@
 from datetime import date
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import random as rand
+import math
+from statistics import mean
+from cycler import cycler
 
 
 class Student():
@@ -31,19 +37,30 @@ def neuron2(value2):
     print(value2)
 
 def neruon3(value3):
-    print(value3))
+    print(value3)
 
 def neuron4(value4):
     print(value4)
 
 def loadData(filename):
     df = pd.read_csv(filename)
-	# splitPercent_title = (splitPercent * 100)
-	# title = "{} {}%".format(title,splitPercent_title)
-    # df.columns = [,]
-    return df;
+    df.columns = ["x1","x2"]
+    return df
 
+def normalization(data):
+    data['x1'] = (data['x1'] - data['x1'].min())/ (data['x1'].max() - data['x1'].min())
+    data['x2'] = (data['x2'] - data['x2'].min())/ (data['x2'].max() - data['x2'].min())
+    return data
 
+def plotPoints(listx,listy, df):
+    sns.lmplot(x = 'x1', y = 'x2', data = df, palette = 'plasma', fit_reg = False).fig.suptitle("xy")
+    # plt.plot(listx, listy)
+    plt.show()
+
+def randomize_weights ():
+    num = 0.5
+    weights = [rand.uniform(0,1) for x in range(3)]
+    return weights
 	# df.columns = ['height', 'weight', 'sex']
 
 # .....................
@@ -65,7 +82,26 @@ def main():
     chady.show()
 
     df = loadData(file_name)
-    
+
+    # print(df)
+    normalization(df)
+    # print(df)
+
+
+    # print(df)
+    # convert to lists
+    x_list = list()
+    y_list = list()
+    x_list = df['x1'].tolist()
+    y_list = df['x2'].tolist()
+
+    plotPoints(x_list,y_list,df)
+    weights = randomize_weights()
+    print(weights)
+    # print(x_list)
+    # print(y_list)
+
+
 
 
 
